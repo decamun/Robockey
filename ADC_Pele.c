dc6a8faa273a597b1096104896c0f56e850a528b
pelec <pelec@seas.upenn.edu>
2015-11-23 12:38:02 -0500

    Added ADC code

---
+++ ADC_Pele.c
@@ -0,0 +1,159 @@
+//------------------------------------------Multiple ADC conversion stuff--------------------------------------------
+
+//ADC intialization function
+
+#include "m_general.h"
+
+void ADC_init(void)
+{
+	clear(ADMUX, REFS1);    // Vref to Vcc
+	set(ADMUX, REFS0);      // ^
+	set(ADCSRA, ADPS2);     // Prescaler of 128 to give 125 kHz clock
+	set(ADCSRA, ADPS1);     // ^
+	set(ADCSRA, ADPS0);     // ^
+
+
+}
+
+
+void ADC0(void)
+{
+    clear(ADCSRA, ADEN);
+    
+    set(DIDR0, ADC0D);                  // F0
+    set(PORTF,0);
+    
+    clear(ADCSRB, MUX5);                // Channel Selection - F0
+    clear(ADMUX, MUX2);                 // ^
+    clear(ADMUX, MUX1);                 // ^
+    clear(ADMUX, MUX0);                 // ^
+    
+    set(ADCSRA, ADEN);					// Enable ADC
+    set(ADCSRA, ADSC);					// Begin Conversion
+    while(!check(ADCSRA,ADIF)){}		// wait for flag to be set
+    set(ADCSRA,ADIF);					// reset the flag
+}
+
+void ADC1(void)
+{
+    clear(ADCSRA, ADEN);
+    
+    set(DIDR0, ADC1D);                  // F1
+    set(PORTF,1);
+    
+    clear(ADCSRB, MUX5);                // Channel Selection - F1
+    clear(ADMUX, MUX2);                 // ^
+    clear(ADMUX, MUX1);                 // ^
+    set(ADMUX, MUX0);                   // ^
+    
+    set(ADCSRA, ADEN);					// Enable ADC
+    set(ADCSRA, ADSC);					// Begin Conversion
+    while(!check(ADCSRA,ADIF)){}		// wait for flag to be set
+    set(ADCSRA,ADIF);					// reset the flag
+}
+
+void ADC4(void)
+{
+    clear(ADCSRA, ADEN);
+    
+    set(DIDR0, ADC4D);                  // F4
+       clear(PORTF,4);
+    
+    clear(ADCSRB, MUX5);                // Channel Selection - F4
+    set(ADMUX, MUX2);                   // ^
+    clear(ADMUX, MUX1);                 // ^
+    clear(ADMUX, MUX0);                 // ^
+    
+    set(ADCSRA, ADEN);					// Enable ADC
+    set(ADCSRA, ADSC);					// Begin Conversion
+    while(!check(ADCSRA,ADIF)){}		// wait for flag to be set
+    set(ADCSRA,ADIF);					// reset the flag
+}
+
+void ADC5(void)
+{
+    clear(ADCSRA, ADEN);
+    
+    set(DIDR0, ADC5D);                  // F5
+    
+    clear(ADCSRB, MUX5);                // Channel Selection - F5
+    set(ADMUX, MUX2);                   // ^
+    clear(ADMUX, MUX1);                 // ^
+    set(ADMUX, MUX0);                   // ^
+    
+    set(ADCSRA, ADEN);					// Enable ADC
+    set(ADCSRA, ADSC);					// Begin Conversion
+    while(!check(ADCSRA,ADIF)){}		// wait for flag to be set
+    set(ADCSRA,ADIF);					// reset the flag
+}
+
+void ADC6(void)
+{
+	clear(ADCSRA, ADEN);
+    
+	set(DIDR0, ADC6D);                  // F6
+    
+    clear(ADCSRB, MUX5);                // Channel Selection - F6
+    set(ADMUX, MUX2);                   // ^
+    set(ADMUX, MUX1);                   // ^
+    clear(ADMUX, MUX0);                 // ^
+    
+	set(ADCSRA, ADEN);					// Enable ADC
+	set(ADCSRA, ADSC);					// Begin Conversion
+	while(!check(ADCSRA,ADIF)){}		// wait for flag to be set
+	set(ADCSRA,ADIF);					// reset the flag
+}
+
+void ADC7(void)
+{
+	clear(ADCSRA, ADEN);
+    
+	set(DIDR0, ADC7D);                  // F7
+    
+    clear(ADCSRB, MUX5);                // Channel Selection - F7
+    set(ADMUX, MUX2);                   // ^
+    set(ADMUX, MUX1);                   // ^
+    set(ADMUX, MUX0);                   // ^
+    
+	set(ADCSRA, ADEN);					// Enable ADC
+	set(ADCSRA, ADSC);					// Begin Conversion
+	while(!check(ADCSRA,ADIF)){}		// wait for flag to be set
+	set(ADCSRA,ADIF);					// reset the flag
+}
+
+
+void ADC8(void)
+{
+    clear(ADCSRA, ADEN);
+    
+    set(DIDR2, ADC8D);                  // D4
+        set(PORTD,4);
+    
+    set(ADCSRB, MUX5);                  // Channel Selection - D4
+    clear(ADMUX, MUX2);                 // ^
+    clear(ADMUX, MUX1);                 // ^
+    clear(ADMUX, MUX0);                 // ^
+    
+    set(ADCSRA, ADEN);                  // Enable ADC
+    set(ADCSRA, ADSC);                  // Begin Conversion
+    while(!check(ADCSRA,ADIF)){}        // wait for flag to be set
+    set(ADCSRA,ADIF);                   // reset the flag
+}
+
+void ADC9(void)
+{
+    clear(ADCSRA, ADEN);
+    
+    set(DIDR2, ADC9D);                  // D6
+        set(PORTD,6);
+    
+    set(ADCSRB, MUX5);                  // Channel Selection - D6
+    clear(ADMUX, MUX2);                 // ^
+    clear(ADMUX, MUX1);                 // ^
+    set(ADMUX, MUX0);                   // ^
+    
+    set(ADCSRA, ADEN);                  // Enable ADC
+    set(ADCSRA, ADSC);                  // Begin Conversion
+    while(!check(ADCSRA,ADIF)){}        // wait for flag to be set
+    set(ADCSRA,ADIF);                   // reset the flag
+}
---
+++ ADC_Pele.h
@@ -0,0 +1,17 @@
+//Analog To Digital Conversion
+
+
+#include "m_general.h"
+
+//------------Initialising ADC functions-----------------//
+void ADC_init(void);
+
+//------------Individual callouts------------------------//
+void ADC0(void); // F0
+void ADC1(void); // F1
+//void ADC2(void);
+//void ADC3(void);
+void ADC4(void); // F4
+void ADC5(void); // F5
+void ADC6(void); // F6
+void ADC7(void); // F7
