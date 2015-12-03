2dcf8f06b0ecd81a8a662777e41ed9f1e4371468
decamun <decamun@gmail.com>
2015-12-02 23:16:20 -0500

    took out usb debug and removed front sensors

--- main.c
+++ main.c
@@ -98,12 +98,12 @@ void main()
             update_puck_angle();
             float angle = get_puck_angle();
 
-            m_usb_tx_string("State ");
-            m_usb_tx_int(current_state);
-            m_usb_tx_string("\r\n");
-            m_usb_tx_string("Puck ");
-            m_usb_tx_int((int) (100 * get_puck_angle()));
-            m_usb_tx_string("\r\n");
+            // m_usb_tx_string("State ");
+            // m_usb_tx_int(current_state);
+            // m_usb_tx_string("\r\n");
+            // m_usb_tx_string("Puck ");
+            // m_usb_tx_int((int) (100 * get_puck_angle()));
+            // m_usb_tx_string("\r\n");
 
             switch(current_state) {
                 case PAUSE:
@@ -171,12 +171,12 @@ void main()
                     } else if (puck_left()) {
                         setLeft(0.8);
                     } else if (puck_middle()) {
-                        current_state = GOTO_GOAL; 
+                        current_state = GOTO_GOAL;
                     } else {
                         current_state = ACQUIRE;
                     }
             }
-           
+
             // We're done until the next clock update
             TICK_HAPPENED = 0;
         }
