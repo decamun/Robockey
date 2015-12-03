cdc24c1e1cb299c754cb82169d1e3bc2a8db0d5c
decamun <decamun@gmail.com>
2015-12-03 01:58:44 -0500

    added get_puck_distance()
    returns puck distance in cm

--- main.c
+++ main.c
@@ -98,12 +98,12 @@ void main()
             update_puck_angle();
             float angle = get_puck_angle();
 
-            // m_usb_tx_string("State ");
-            // m_usb_tx_int(current_state);
-            // m_usb_tx_string("\r\n");
-            // m_usb_tx_string("Puck ");
-            // m_usb_tx_int((int) (100 * get_puck_angle()));
-            // m_usb_tx_string("\r\n");
+            m_usb_tx_string("State ");
+            m_usb_tx_int(current_state);
+            m_usb_tx_string("\r\n");
+            m_usb_tx_string("Puck ");
+            m_usb_tx_int((int) (100 * get_puck_angle()));
+            m_usb_tx_string("\r\n");
 
             switch(current_state) {
                 case PAUSE:
@@ -234,7 +234,7 @@ void initialize() {
 
     //enable kicker ports
     set(DDRB, 7);
-    cleat(PORTB, 7);
+    clear(PORTB, 7);
 
     //enable inputs for switches
     clear(DDRB, 4);
